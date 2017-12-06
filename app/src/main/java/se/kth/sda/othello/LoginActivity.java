f70771d6633e4157c350899557f1ff44b7c8b354
armin.dizdar@gmail.com <jebenekipasvord1>
2017-12-06 17:17:44 +0100

    Changed appearance of buttons. Logo on first activity is now rotating for 3 seconds.

--- app/src/main/java/se/kth/sda/othello/LoginActivity.java
+++ app/src/main/java/se/kth/sda/othello/LoginActivity.java
@@ -3,6 +3,9 @@ package se.kth.sda.othello;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 import se.kth.sda.othello.imp.OthelloFactoryImp;
 
@@ -21,10 +24,10 @@ public class LoginActivity extends Activity {
         begin();
     }
 
-
-    // Method for going to second activity which is delayed by 3 seconds.
+    // Multithreading
+    // Going to second activity which is delayed by 3 seconds.
+    // Calling rotate method.
     // By Armin Dizdar 05/12/2017
-
     public void begin() {
         new Thread(new Runnable() {
             @Override
@@ -38,6 +41,23 @@ public class LoginActivity extends Activity {
                 }
             }
         }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                rotate();
+            }
+        }).start();
+
+
+    }
+
+    // Rotating logo image by 360 degrees in 3 seconds.
+    // By Armin Dizdar 06/12/2017
+    public void rotate(){
+        RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(3000);
+        ImageView logo = (ImageView) findViewById(R.id.logo_image);
+        logo.startAnimation(rotate);
     }
 
     @Override
